ebf1f0584b0b04b747f204578bf67f3fef3274a5
D18057chenziping <chenzp139@tydic.com>
2025-06-05 14:01:41 +0800

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/manus-dynamic-section',
+  base: '/manus-dynamic-section/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
